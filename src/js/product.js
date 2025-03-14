226fffb23396c3f59997c5a1d8da7e703be7b889
cristiangsmiranda <san24063@byui.edu>
2025-03-13 22:50:21 -0300

    w02 learn activity added

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,17 @@
-import { getParam } from "./utils.mjs";
+
+import { getLocalStorage, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
 
-const productId = getParam("product");  // Obtém o ID do produto da URL
-const dataSource = new ProductData("tents");  // Fonte de dados do produto
+const dataSource = new ProductData("tents");
+const productId = getParam("product");
 
-const product = new ProductDetails(productId, dataSource);
-product.init();  // Inicializa a exibição do produto
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
+
+// add listener to Add to Cart button
+document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
